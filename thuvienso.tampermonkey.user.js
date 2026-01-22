124b8367f3266e731ab88eb368b69e959f577b62
Htu12 <Workspace.dev.huutu1203@gmail.com>
2026-01-23 02:42:03 +0700

    Update metadata in APD PDF Downloader script

--- thuvienso.tampermonkey.user.js
+++ thuvienso.tampermonkey.user.js
@@ -3,9 +3,14 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.2
 // @description  Tự động tìm hash, lọc mã rác và tải PDF từ thuvienso.apd.edu.vn
-// @author       Huu Tu  
-// @icon         https://images.icon-icons.com/410/PNG/128/docs_40849.png
+// @author       Huu Tu
+// @require      https://raw.githubusercontent.com/Htu12/tools/refs/heads/main/download_doc.js
+// @homepageURL  https://github.com/Htu12/tools
+// @downloadURL  https://github.com/Htu12/tools/blob/main/thuvienso.tampermonkey.user.js
+// @updateURL    https://github.com/Htu12/tools/blob/main/thuvienso.tampermonkey.user.js
 // @match        https://thuvienso.apd.edu.vn/*
 // @exclude      https://thuvienso.apd.edu.vn/tvs/*
+// @run-at       document-idle
 // @grant        none
+// @icon         https://images.icon-icons.com/410/PNG/128/docs_40849.png
 // ==/UserScript==
