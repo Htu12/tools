3b1d87ce8f6d1b711e04b03fe814fd4751131d56
Htu12 <Workspace.dev.huutu1203@gmail.com>
2026-01-23 02:36:26 +0700

    Add initial user script for APD PDF downloading functionality

---
+++ thuvienso.tampermonkey.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         APD PDF Downloader
+// @namespace    http://tampermonkey.net/
+// @version      1.2
+// @description  Tự động tìm hash, lọc mã rác và tải PDF từ thuvienso.apd.edu.vn
+// @author       Huu Tu  
+// @icon         https://images.icon-icons.com/410/PNG/128/docs_40849.png
+// @match        https://thuvienso.apd.edu.vn/*
+// @exclude      https://thuvienso.apd.edu.vn/tvs/*
+// @grant        none
+// ==/UserScript==
